2f1885f06e102f240a0bfbba803c6c38cf2971ad
joao_vitorg <jovitor737@gmail.com>
2022-10-31 00:53:18 -0300

    Model e DTOs remodelados

--- src/main/java/com/example/padroesprojetos/model/endereco/Endereco.java
+++ src/main/java/com/example/padroesprojetos/model/endereco/Endereco.java
@@ -21,8 +21,5 @@ public class Endereco {
 	private String bairro;
 	private String localidade;
 	private String uf;
-	private String ibge;
-	private String gia;
 	private String ddd;
-	private String siafi;
 }
