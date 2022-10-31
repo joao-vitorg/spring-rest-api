c88039ec192314ac5d32227f54b6db6599dca82b
joao_vitorg <jovitor737@gmail.com>
2022-10-31 01:25:50 -0300

    Implementação e atualização dos métodos e error handlers

--- src/main/java/com/example/padroesprojetos/controller/ClienteRestController.java
+++ src/main/java/com/example/padroesprojetos/controller/ClienteRestController.java
@@ -1,49 +1,50 @@
 package com.example.padroesprojetos.controller;
 
+import com.example.padroesprojetos.handler.ResponseError;
 import com.example.padroesprojetos.model.cliente.Cliente;
-import com.example.padroesprojetos.service.ClienteInterface;
+import com.example.padroesprojetos.model.cliente.ClienteDto;
+import com.example.padroesprojetos.service.ClienteService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("cliente")
 public class ClienteRestController {
-    private final ClienteInterface clienteInterface;
+    private final ClienteService clienteService;
 
-    public ClienteRestController(ClienteInterface clienteInterface) {
-        this.clienteInterface = clienteInterface;
+    public ClienteRestController(ClienteService clienteService) {
+        this.clienteService = clienteService;
     }
 
     @GetMapping
     public ResponseEntity<Iterable<Cliente>> index() {
-        return ResponseEntity.ok(clienteInterface.buscarTodos());
+        return ResponseEntity.ok(clienteService.findAll());
     }
 
-    /**
-     * d
-     *
-     * @param cliente b
-     * @return c
-     * @apiNote a
-     */
     @PostMapping
-    public ResponseEntity<Cliente> create(@RequestBody Cliente cliente) {
-        clienteInterface.inserir(cliente);
-        return ResponseEntity.ok(cliente);
+    public ResponseEntity<Cliente> create(@RequestBody ClienteDto cliente) {
+        return ResponseEntity.ok(clienteService.insert(cliente));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Cliente> getByID(@PathVariable Long id) {
-        return ResponseEntity.of(clienteInterface.buscarPorId(id));
+        return ResponseEntity.ok(clienteService.findById(id));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Void> updateByID(@PathVariable Long id, @RequestBody Cliente cliente) {
-        return (clienteInterface.atualizar(id, cliente) ? ResponseEntity.ok() : ResponseEntity.badRequest()).build();
+    public ResponseEntity<Cliente> updateByID(@PathVariable Long id, @RequestBody ClienteDto cliente) {
+        return ResponseEntity.ok(clienteService.update(id, cliente));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteByID(@PathVariable Long id) {
-        return (clienteInterface.deletar(id) ? ResponseEntity.ok() : ResponseEntity.badRequest()).build();
+        clienteService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<ResponseError> exceptionHandler(RuntimeException err) {
+        ResponseError responseError = new ResponseError(err.getMessage());
+        return ResponseEntity.badRequest().body(responseError);
     }
 }
